66fd017f228be9eddd58e91940218af0cf66323c
sungjinlee <qdlsungls@gmail.com>
2023-08-24 18:18:42 +0900

    230824

---
+++ Baekjoon Online Judge/1629.py
@@ -0,0 +1,13 @@
+A, B, C = map(int, input().split())
+
+
+def recursion(a, b, c):
+    if b == 1:
+        return a % c
+    if b % 2 == 1:
+        return recursion(a * a % c, b // 2, c) * a % c
+    else:
+        return recursion(a * a % c, b // 2, c) % c
+
+
+print(recursion(A, B, C))

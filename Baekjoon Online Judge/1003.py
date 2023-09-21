603e8c413a6960a397522cf79143fd73669982ad
sungjinlee <qdlsungls@gmail.com>
2023-09-21 17:46:51 +0900

    230921

--- Baekjoon Online Judge/1003.py
+++ Baekjoon Online Judge/1003.py
@@ -1,15 +1,12 @@
-# 구현
-# 다이나믹 프로그래밍
+import sys
 
-T = int(input())
+input = sys.stdin.readline
 
-fibonacci = [(1, 0), (0, 1)]
-
-for n in range(2, 41):
-    fibonacci.append(
-        (fibonacci[n-2][0] + fibonacci[n-1][0], fibonacci[n-2][1] + fibonacci[n-1][1]))
-
+fibonacci = [0] * 41
+fibonacci[0] = (1, 0)
+fibonacci[1] = (0, 1)
+for i in range(2, 41):
+    fibonacci[i] = (fibonacci[i - 1][0] + fibonacci[i - 2][0], fibonacci[i - 1][1] + fibonacci[i - 2][1])
+T = int(input().rstrip())
 for _ in range(T):
-    n = int(input())
-
-    print(fibonacci[n][0], fibonacci[n][1])
+    print(*fibonacci[int(input().rstrip())])

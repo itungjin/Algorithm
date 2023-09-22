b25c6ddaf49128529cf6f824c054d157217b2d66
sungjinlee <qdlsungls@gmail.com>
2023-09-23 04:50:43 +0900

    230923

---
+++ Baekjoon Online Judge/2156.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+arr = [int(input().rstrip()) for _ in range(n)]
+
+if n <= 2:
+    print(sum(arr))
+    exit(0)
+dp = [0 for _ in range(n)]
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+dp[2] = max(dp[1], arr[2] + dp[0])
+for i in range(2, n):
+    dp[i] = max(dp[i - 1], arr[i] + dp[i - 2], arr[i] + dp[i - 3] + arr[i - 1])
+print(dp[n - 1])

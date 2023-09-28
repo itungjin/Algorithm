9b26a1a09d499e7315cc42c2621595dae25eb6f5
sungjinlee <qdlsungls@gmail.com>
2023-09-28 17:35:24 +0900

    230928

--- Baekjoon Online Judge/1439.py
+++ Baekjoon Online Judge/1439.py
@@ -1,15 +1,11 @@
-# 그리디
-# S는 0과 1로만 이루어진 문자열
-# 연속된 1을 0으로 바꾸거나 연속된 0을 1로 바꾸어 문자열을 한 숫자로만 구성되게 변경
-# 0과 1이 교차된 횟수를 비교하고 규칙을 찾음
+import sys
 
-s = input()
-answer = 0
+input = sys.stdin.readline
 
-chr_now = s[0]
-for i in range(1, len(s)):
-    if (chr_now != s[i]):
-        chr_now = s[i]
-        answer += 1
-
-print((answer + 1) // 2)
+S = input().rstrip()
+count = [0] * 2
+count[int(S[0])] += 1
+for i in range(1, len(S)):
+    if S[i] != S[i - 1]:
+        count[int(S[i])] += 1
+print(min(count))

53dc50369d861830c47da2dcafc1cd10c4d2d174
sungjinlee <qdlsungls@gmail.com>
2023-10-04 23:41:50 +0900

    231004

--- Baekjoon Online Judge/18870.py
+++ Baekjoon Online Judge/18870.py
@@ -1,12 +1,13 @@
 import sys
-
+from bisect import bisect
 input = sys.stdin.readline
 
 N = int(input().rstrip())
 X = list(map(int, input().split()))
-x2 = sorted(list(set(X)))
-x3 = dict()
-for i in range(len(x2)):
-    x3[x2[i]] = i
+x2 = sorted(X)
+x3 = [x2[0]]
+for i in range(1, len(x2)):
+    if x2[i] != x2[i-1]:
+        x3.append(x2[i])
 for x in X:
-    print(x3[x], end=' ')
+    print(bisect(x3, x) - 1, end=' ')

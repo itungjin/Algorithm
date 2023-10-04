eaae51d7076cbf0981b939610bacfc6b5aa11156
sungjinlee <qdlsungls@gmail.com>
2023-10-04 19:15:13 +0900

    231004

--- Baekjoon Online Judge/11652.py
+++ Baekjoon Online Judge/11652.py
@@ -2,24 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-max_value = 0
-max_store = []
-count = dict()
-max_index = 0
-for _ in range(N):
-    num = int(input().rstrip())
-    if num not in count:
-        count[num] = 0
-    if count[num] + 1 > max_value:
-        count[num] += 1
-        max_value = count[num]
-        max_store.clear()
-        max_store = [num]
-    elif count[num] + 1 == max_value:
-        count[num] += 1
-        max_store.append(num)
-    else:
-        count[num] += 1
-max_store.sort()
-print(max_store[0])
+N = int(input().rstrip())
+nums = [int(input().rstrip()) for _ in range(N)]
+nums.sort()
+max_value = nums[0]
+max_count = 1
+count = 1
+for i in range(1, N):
+    if nums[i] != nums[i-1]:
+        if count > max_count:
+            max_value = nums[i-1]
+            max_count = count
+        count = 0
+    count += 1
+if count > max_count:
+    max_value = nums[-1]
+print(max_value)

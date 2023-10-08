2fbc612816d62eb265fb312866f4ea5d831d5c89
sungjinlee <qdlsungls@gmail.com>
2023-10-09 02:50:03 +0900

    231009

--- Baekjoon Online Judge/9251.py
+++ Baekjoon Online Judge/9251.py
@@ -4,34 +4,22 @@ input = sys.stdin.readline
 
 s1 = input().rstrip()
 s2 = input().rstrip()
-n1 = len(s1)
-n2 = len(s2)
-dp = [[0 for _ in range(n2)] for _ in range(n1)]
-answer = 0
-mapping = [[] for _ in range(n1)]
-for i in range(n1):
+dp = [[0 for _ in range(len(s2))] for _ in range(len(s1))]
+for i in range(0, len(s2)):
+    if s1[0] == s2[i]:
+        for j in range(i, len(s2)):
+            dp[0][j] = 1
+        break
+for i in range(0, len(s1)):
     if s1[i] == s2[0]:
-        answer = 1
-        dp[i][0] = 1
-    for j in range(1, n2):
+        for j in range(i, len(s1)):
+            dp[j][0] = 1
+        break
+
+for i in range(1, len(s1)):
+    for j in range(1, len(s2)):
         if s1[i] == s2[j]:
-            mapping[i].append(j)
-            answer = 1
-            dp[i][j] = 1
-        elif dp[i][j - 1] == 1:
-            dp[i][j] = 1
-for i in range(1, n1):
-    temp = dp[i - 1][0]
-    if dp[i][0] < temp:
-        dp[i][0] = temp
-    for j in range(1, n2):
-        if s1[i] == s2[j]:
-            if max(dp[i - 1][j - 1] + 1, dp[i - 1][j]) > temp:
-                temp = max(dp[i - 1][j - 1] + 1, dp[i - 1][j])
+            dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j - 1], dp[i - 1][j])
         else:
-            if dp[i - 1][j] > temp:
-                temp = dp[i - 1][j]
-        if dp[i][j] < temp:
-            dp[i][j] = temp
-answer = max(answer, dp[n1 - 1][n2 - 1])
-print(answer)
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+print(dp[len(s1) - 1][len(s2) - 1])

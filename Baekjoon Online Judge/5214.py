e2642192af0f10cbcf78d1f924dd310057df03c4
sungjinlee <qdlsungls@gmail.com>
2023-10-11 23:02:19 +0900

    231011

--- Baekjoon Online Judge/5214.py
+++ Baekjoon Online Judge/5214.py
@@ -4,20 +4,23 @@ from collections import deque
 input = sys.stdin.readline
 
 N, K, M = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-for _ in range(M):
-    stations = list(map(int, input().split()))
-    for i in range(len(stations)):
-        for j in range(i, len(stations)):
-            graph[stations[i]].append(stations[j])
-            graph[stations[j]].append(stations[i])
-distance = [-1] * (N + 1)
+hypercube = [[] for _ in range(M)]
+connected = [[] for _ in range(N + 1)]
+distance = [-1 for _ in range(N + 1)]
 distance[1] = 1
+checked = [False] * M
+for i in range(M):
+    hypercube[i] = list(map(int, input().split()))
+    for station in hypercube[i]:
+        connected[station].append(i)
 q = deque([1])
 while q:
     v = q.popleft()
-    for u in graph[v]:
-        if distance[u] == -1:
-            distance[u] = distance[v] + 1
-            q.append(u)
+    for h in connected[v]:
+        if not checked[h]:
+            checked[h] = True
+            for u in hypercube[h]:
+                if distance[u] == -1:
+                    distance[u] = distance[v] + 1
+                    q.append(u)
 print(distance[N])

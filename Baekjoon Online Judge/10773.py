000d08c52ca31bffd8dd61d68ec2d26cf8814564
sungjinlee <qdlsungls@gmail.com>
2023-10-14 18:43:26 +0900

    231014

--- Baekjoon Online Judge/10773.py
+++ Baekjoon Online Judge/10773.py
@@ -1,19 +1,19 @@
-# 스택
-
 import sys
-from collections import deque
+
 input = sys.stdin.readline
 
-recent = deque()
-total = 0
+stack = [0] * 100000
+top = 0
 
 K = int(input().rstrip())
 for _ in range(K):
-    integer = int(input().rstrip())
-    if integer == 0:
-        total -= recent.pop()
+    num = int(input().rstrip())
+    if num == 0:
+        top -= 1
     else:
-        recent.append(integer)
-        total += integer
-
-print(total)
+        stack[top] = num
+        top += 1
+ans = 0
+for i in range(0, top):
+    ans += stack[i]
+print(ans)

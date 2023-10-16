644b31f9a8353e051a423e91d8f778106b926436
sungjinlee <qdlsungls@gmail.com>
2023-10-16 21:03:15 +0900

    231015

--- Baekjoon Online Judge/17298.py
+++ Baekjoon Online Judge/17298.py
@@ -5,10 +5,13 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 A = list(map(int, input().split()))
 stack = []
-answer = [-1] * N
-
-for i in range(N):
-    while stack and A[stack[-1]] < A[i]:
-        answer[stack.pop()] = A[i]
-    stack.append(i)
+answer = [0] * N
+for i in range(N - 1, -1, -1):
+    while stack and stack[-1] <= A[i]:
+        stack.pop()
+    if not stack:
+        answer[i] = -1
+    else:
+        answer[i] = stack[-1]
+    stack.append(A[i])
 print(*answer)

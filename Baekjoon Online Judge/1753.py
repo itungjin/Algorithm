519589969746595924f15c6aca5ecd7b89b68a18
sungjinlee <qdlsungls@gmail.com>
2023-10-20 11:13:37 +0900

    231020

---
+++ Baekjoon Online Judge/1753.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+V, E = map(int, input().split())
+K = int(input().rstrip())
+graph = [[] for _ in range(V + 1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+dist = [INF] * (V + 1)
+dist[K] = 0
+heap = [(K, 0)]
+while heap:
+    v, d = heapq.heappop(heap)
+    if d < dist[v]:
+        continue
+    for nv, nw in graph[v]:
+        if d + nw < dist[nv]:
+            dist[nv] = d + nw
+            heapq.heappush(heap, (nv, d + nw))
+for i in range(1, V + 1):
+    if dist[i] == INF:
+        print('INF')
+    else:
+        print(dist[i])

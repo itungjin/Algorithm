ea50de903976607d7b0e99ef8b5ceb29d427914c
sungjinlee <qdlsungls@gmail.com>
2023-10-20 17:33:59 +0900

    231020

--- Baekjoon Online Judge/1261.py
+++ Baekjoon Online Judge/1261.py
@@ -8,15 +8,16 @@ M, N = map(int, input().split())
 maze = [list(map(int, input().rstrip())) for _ in range(N)]
 heap = [(0, 0, 0)]
 cnt = [[INF] * M for _ in range(N)]
-moves = ((1, 0), (0, 1))
+cnt[0][0] = 0
+move = ((1, 0), (0, 1), (-1, 0), (0, -1))
 while heap:
     c, n, m = heapq.heappop(heap)
     if c != cnt[n][m]:
         continue
-    for dn, dm in moves:
+    for dn, dm in move:
         nn = n + dn
         nm = m + dm
-        if nn >= N or nm >= M:
+        if nn >= N or nm >= M or nn < 0 or nm < 0:
             continue
         if c + maze[nn][nm] < cnt[nn][nm]:
             cnt[nn][nm] = c + maze[nn][nm]

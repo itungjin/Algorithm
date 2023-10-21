934040053b3167a12bcac69e8b77a54907eaed26
sungjinlee <qdlsungls@gmail.com>
2023-10-21 12:16:29 +0900

    231020

--- 이것이 취업을 위한 코딩 테스트다/만들 수 없는 금액.py
+++ 이것이 취업을 위한 코딩 테스트다/만들 수 없는 금액.py
@@ -5,9 +5,11 @@ input = sys.stdin.readline
 N = int(input().rstrip())
 coins = list(map(int, input().split()))
 coins.sort()
+# ans - 1까지의 금액은 만들 수 있다.
 ans = 1
 for coin in coins:
     if coin > ans:
         break
+    # ans - 1 + coin까지의 금액은 만들 수 있다.
     ans += coin
 print(ans)

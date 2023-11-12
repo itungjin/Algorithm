9974f8479064adf523e30a83f412c74d7484b5b3
sungjinlee <qdlsungls@gmail.com>
2023-11-13 08:39:13 +0900

    231113

--- Baekjoon Online Judge/2075.py
+++ Baekjoon Online Judge/2075.py
@@ -4,15 +4,16 @@ import heapq
 input = sys.stdin.readline
 
 N = int(input().rstrip())
-heap = list()
-divisor = 1
-for i in range(N, 0, -1):
-    a = list(map(int, input().split()))
-    a.sort(reverse=True)
-    while i == N // (divisor + 1):
-        divisor += 1
-    for j in range(divisor):
-        heapq.heappush(heap, -a[j])
-for i in range(N - 1):
-    heapq.heappop(heap)
-print(-heapq.heappop(heap))
+heap = []
+row = list(map(int, input().split()))
+row.sort()
+for number in row:
+    heapq.heappush(heap, number)
+for _ in range(N - 1):
+    row = list(map(int, input().split()))
+    row.sort(reverse=True)
+    for number in row:
+        if number < heap[0]:
+            break
+        heapq.heappushpop(heap, number)
+print(heapq.heappop(heap))

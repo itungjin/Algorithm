2f87786ab8ee3b31e47d5b5859931498b04648ac
sungjinlee <qdlsungls@gmail.com>
2023-11-13 09:13:14 +0900

    231113

--- Baekjoon Online Judge/13975.py
+++ Baekjoon Online Judge/13975.py
@@ -8,10 +8,11 @@ for _ in range(T):
     K = int(input().rstrip())
     files = list(map(int, input().split()))
     heapq.heapify(files)
-    cost = 0
+
+    answer = 0
     while len(files) != 1:
-        f1, f2 = heapq.heappop(files), heapq.heappop(files)
-        f3 = f1 + f2
-        cost += f3
-        heapq.heappush(files, f3)
-    print(cost)
+        temp_file = heapq.heappop(files) + heapq.heappop(files)
+        heapq.heappush(files, temp_file)
+        answer += temp_file
+
+    print(answer)

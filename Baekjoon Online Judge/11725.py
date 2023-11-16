415cc1df79e1a523c33289ede02d1a6c34dd006c
sungjinlee <qdlsungls@gmail.com>
2023-11-17 02:31:30 +0900

    231117

--- Baekjoon Online Judge/11725.py
+++ Baekjoon Online Judge/11725.py
@@ -5,18 +5,19 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 graph = [[] for _ in range(N + 1)]
-parent = [-1] * (N + 1)
-parent[1] = 0
 for _ in range(N - 1):
-    v1, v2 = map(int, input().split())
-    graph[v1].append(v2)
-    graph[v2].append(v1)
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+parent = [0] * (N + 1)
+parent[1] = -1
 q = deque([1])
 while q:
-    p = q.popleft()
-    for c in graph[p]:
-        if parent[c] == -1:
-            parent[c] = p
-            q.append(c)
+    u = q.popleft()
+    for v in graph[u]:
+        if parent[v] == 0:
+            parent[v] = u
+            q.append(v)
+
 for i in range(2, N + 1):
     print(parent[i])

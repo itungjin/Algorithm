44eb5b17421e5ee6a0c49e5b4376b6ce0c596073
sungjinlee <qdlsungls@gmail.com>
2023-11-17 02:00:06 +0900

    231117

--- Baekjoon Online Judge/2606.py
+++ Baekjoon Online Judge/2606.py
@@ -3,22 +3,24 @@ from collections import deque
 
 input = sys.stdin.readline
 
-graph = [[] for _ in range(101)]
-nv = int(input().rstrip())
-ne = int(input().rstrip())
-for _ in range(ne):
+n = int(input().rstrip())
+m = int(input().rstrip())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
-visited = [False] * 101
-visited[1] = True
 answer = 0
+visited = [False] * (n + 1)
+visited[1] = True
 q = deque([1])
 while q:
-    v = q.popleft()
-    for u in graph[v]:
-        if not visited[u]:
-            q.append(u)
-            visited[u] = True
-            answer += 1
+    u = q.popleft()
+    for v in graph[u]:
+        if visited[v]:
+            continue
+        visited[v] = True
+        answer += 1
+        q.append(v)
+
 print(answer)

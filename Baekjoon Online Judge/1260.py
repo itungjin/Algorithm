1df827baa121ba8de343cb6689f077bd9e976ae3
sungjinlee <qdlsungls@gmail.com>
2023-11-17 01:48:51 +0900

    231117

--- Baekjoon Online Judge/1260.py
+++ Baekjoon Online Judge/1260.py
@@ -4,36 +4,44 @@ from collections import deque
 input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+graph = [set() for _ in range(N + 1)]
 for _ in range(M):
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    graph[u].add(v)
+    graph[v].add(u)
 for i in range(1, N + 1):
-    graph[i].sort(reverse = True)
+    graph[i] = sorted(list(graph[i]))
+
+
 visited = [False] * (N + 1)
-s = [V]
-while s:
-    v = s.pop()
-    if visited[v]:
-        continue
-    visited[v] = True
-    print(v, end=' ')
-    for u in graph[v]:
-        if not visited[u]:
-            s.append(u)
-print()
-for i in range(1, N + 1):
-    graph[i].sort()
-visited = [False] * (N + 1)
-q = deque([V])
-print(V, end=' ')
 visited[V] = True
-while q:
-    v = q.popleft()
-    for u in graph[v]:
-        if not visited[u]:
-            q.append(u)
-            print(u, end=' ')
-            visited[u] = True
+
+
+def dfs(u):
+    print(u, end=' ')
+    for v in graph[u]:
+        if not visited[v]:
+            visited[v] = True
+            dfs(v)
+
+
+visited[V] = True
+dfs(V)
 print()
+
+
+def bfs(u):
+    visited = [False] * (N + 1)
+    visited[u] = True
+    q = deque([u])
+    while q:
+        u = q.popleft()
+        print(u, end=' ')
+        for v in graph[u]:
+            if not visited[v]:
+                visited[v] = True
+                q.append(v)
+
+
+bfs(V)
+

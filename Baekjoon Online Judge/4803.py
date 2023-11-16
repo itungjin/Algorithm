addd45ab7b633315f236208089684a58d68f01bc
sungjinlee <qdlsungls@gmail.com>
2023-11-17 03:23:37 +0900

    231117

--- Baekjoon Online Judge/4803.py
+++ Baekjoon Online Judge/4803.py
@@ -3,39 +3,44 @@ from collections import deque
 
 input = sys.stdin.readline
 
-t = 0
+case = 0
 while True:
-    t += 1
+    case += 1
+
     n, m = map(int, input().split())
     if n == 0 and m == 0:
         break
-    parent = [0] * (n + 1)
     graph = [[] for _ in range(n + 1)]
     for _ in range(m):
-        v1, v2 = map(int, input().split())
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-    count = 0
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    visited = [False] * (n + 1)
+    t = 0
     for i in range(1, n + 1):
-        if parent[i] == 0:
-            is_tree = True
-            q = deque([i])
-            while q:
-                cur = q.popleft()
-                for nxt in graph[cur]:
-                    if nxt == parent[cur]:
-                        continue
-                    if parent[nxt] == 0:
-                        q.append(nxt)
-                        parent[nxt] = cur
-                    else:
-                        is_tree = False
-            if is_tree:
-                count += 1
-    print(f'Case {t}: ', end='')
-    if count > 1:
-        print(f'A forest of {count} trees.')
-    elif count == 1:
+        if visited[i]:
+            continue
+        visited[i] = True
+        node = 0
+        edge = 0
+        q = deque([i])
+        while q:
+            u = q.popleft()
+            node += 1
+            for v in graph[u]:
+                edge += 1
+                if visited[v]:
+                    continue
+                visited[v] = True
+                q.append(v)
+        edge //= 2
+        if node == edge + 1:
+            t += 1
+
+    print(f'Case {case}: ', end='')
+    if t == 0:
+        print('No trees.')
+    elif t == 1:
         print('There is one tree.')
     else:
-        print('No trees.')
+        print(f'A forest of {t} trees.')
